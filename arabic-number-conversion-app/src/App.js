98408158e29739ca835066c9e1f1d88f2b21c624
Betti Bodnar <bettinabodnar7@gmail.com>
2022-01-31 21:33:11 +0100

    feat: add input form, states and handleevent functions to App.js to test conversion functions.

--- arabic-number-conversion-app/src/App.js
+++ arabic-number-conversion-app/src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
+import {useState} from "react";
+import {convertArabianNumberToEnglishPhrase} from "./util/Conversion";
 
 function App() {
+    const [arabNumber, setArabNumber] = useState(0);
+    const [result, setResult] = useState("");
+
+    const handleChange = (event) => {
+        setArabNumber(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        let result = convertArabianNumberToEnglishPhrase(arabNumber);
+        setResult(result);
+        event.preventDefault();
+    }
+
+
   return (
-    <h1>My number conversion app</h1>
+      <>
+      <h1>My number conversion app</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Arabic number:
+          <input type="text" pattern="[0-9]*" name="number"
+                 value={arabNumber} onChange={(e) => handleChange(e)}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+          <p>{result}</p>
+      </>
   );
 }
 
