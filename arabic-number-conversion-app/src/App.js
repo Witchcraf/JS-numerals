b3df3f3ae77cf6ef6eab084b5d90ae45ed5b989b
Betti Bodnar <bettinabodnar7@gmail.com>
2022-02-04 23:51:23 +0100

    feat: add style to h1 and rename it.

--- arabic-number-conversion-app/src/App.js
+++ arabic-number-conversion-app/src/App.js
@@ -9,18 +9,15 @@ function App() {
     const [arabNumber, setArabNumber] = useState(0);
     const [result, setResult] = useState("");
 
-
   return (
       <>
           <div className="App" >
               <Matrix />
-              <h1>My number conversion app</h1>
+              <h1 style={{color:"#F5F5DC", fontFamily: 'MATRIX', fontSize:"50px", marginBottom: "50px"}}>The one conversion app</h1>
               <Form arabNumber={arabNumber} setArabNumber={setArabNumber} setResult={setResult}/>
-              <EnglishPhrase arabNumber={arabNumber} result={result}/>
+              <EnglishPhrase result={result}/>
           </div>
       </>
   );
 }
-
-
 export default App;
