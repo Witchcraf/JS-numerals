2a3f0a5d151bab4d92a4bc12503346a0ea6f45cc
Betti Bodnar <bettinabodnar7@gmail.com>
2022-02-04 10:44:11 +0100

    feat: create conversion.test.js to test the given examples.

---
+++ arabic-number-conversion-app/src/util/conversion.test.js
@@ -0,0 +1,42 @@
+import {convertArabianNumberToEnglishPhrase} from "./conversion";
+
+test('7', ()=> {
+    const result = convertArabianNumberToEnglishPhrase(7);
+    expect(result).toBe("seven");
+})
+
+test('10', ()=> {
+    const result = convertArabianNumberToEnglishPhrase(10);
+    expect(result).toBe("ten");
+})
+
+test('42', ()=> {
+    const result = convertArabianNumberToEnglishPhrase(42);
+    expect(result).toBe("forty- two");
+})
+
+test('1999', ()=> {
+    const result = convertArabianNumberToEnglishPhrase(1999);
+    expect(result).toBe("one thousand nine hundred and  ninety- nine");
+})
+
+test('2001', ()=> {
+    const result = convertArabianNumberToEnglishPhrase(2001);
+    expect(result).toBe("two thousand and  one");
+})
+
+test('17999', ()=> {
+    const result = convertArabianNumberToEnglishPhrase(17999);
+    expect(result).toBe("seventeen thousand nine hundred and  ninety- nine");
+})
+
+test('342251', ()=> {
+    const result = convertArabianNumberToEnglishPhrase(342251);
+    expect(result).toBe("three hundred and  forty- two thousand two hundred and  fifty- one");
+})
+
+test('1300420', ()=> {
+    const result = convertArabianNumberToEnglishPhrase(1300420);
+    expect(result).toBe("one million three hundred  thousand four hundred and  twenty ");
+})
+
