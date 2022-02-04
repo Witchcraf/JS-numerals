c1ed03d82789d4baa35b38b6016d65a53f660846
Betti Bodnar <bettinabodnar7@gmail.com>
2022-02-04 10:46:24 +0100

    feat: add tests to app.test.js to test react components.

--- arabic-number-conversion-app/src/App.test.js
+++ arabic-number-conversion-app/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('basic user flow',async ()=>{
+    render(<App />);
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, {target:{value:7}});
+    const form = screen.getByTestId('conversionform');
+    fireEvent.submit(form);
+    const text = await screen.findByText("7 = seven");
+    expect(text).toBeInTheDocument();
+})
+
+test('ontest event fired', ()=>{
+    const testHandler = jest.fn();
+    render(<App onTestEvent={testHandler} />);
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, {target:{value:7}});
+    const form = screen.getByTestId('conversionform');
+    fireEvent.submit(form);
+    expect(testHandler).toBeCalled();
+})
+
+// empty form, issue handling etc.
