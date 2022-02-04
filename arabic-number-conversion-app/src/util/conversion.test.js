d91db5daf7114453ba6cc5cc81cb7a05f7e2a64c
Betti Bodnar <bettinabodnar7@gmail.com>
2022-02-04 12:01:39 +0100

    fix: fix expected values in tests.

--- arabic-number-conversion-app/src/util/conversion.test.js
+++ arabic-number-conversion-app/src/util/conversion.test.js
@@ -12,31 +12,31 @@ test('10', ()=> {
 
 test('42', ()=> {
     const result = convertArabianNumberToEnglishPhrase(42);
-    expect(result).toBe("forty- two");
+    expect(result).toBe("forty-two");
 })
 
 test('1999', ()=> {
     const result = convertArabianNumberToEnglishPhrase(1999);
-    expect(result).toBe("one thousand nine hundred and  ninety- nine");
+    expect(result).toBe("one thousand nine hundred and ninety-nine");
 })
 
 test('2001', ()=> {
     const result = convertArabianNumberToEnglishPhrase(2001);
-    expect(result).toBe("two thousand and  one");
+    expect(result).toBe("two thousand and one");
 })
 
 test('17999', ()=> {
     const result = convertArabianNumberToEnglishPhrase(17999);
-    expect(result).toBe("seventeen thousand nine hundred and  ninety- nine");
+    expect(result).toBe("seventeen thousand nine hundred and ninety-nine");
 })
 
 test('342251', ()=> {
     const result = convertArabianNumberToEnglishPhrase(342251);
-    expect(result).toBe("three hundred and  forty- two thousand two hundred and  fifty- one");
+    expect(result).toBe("three hundred and forty-two thousand two hundred and fifty-one");
 })
 
 test('1300420', ()=> {
     const result = convertArabianNumberToEnglishPhrase(1300420);
-    expect(result).toBe("one million three hundred  thousand four hundred and  twenty ");
+    expect(result).toBe("one million three hundred thousand four hundred and twenty");
 })
 
