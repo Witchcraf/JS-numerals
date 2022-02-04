7b0eec1c7865d92602e20ac27d3c3b52508e504c
Betti Bodnar <bettinabodnar7@gmail.com>
2022-02-04 21:44:16 +0100

    feat: create englishphrase component to display the result phrase.

---
+++ arabic-number-conversion-app/src/components/englishphrase.js
@@ -0,0 +1,10 @@
+function EnglishPhrase({result,arabNumber}){
+    return (
+        <div className="result">
+          <p>{arabNumber +" = "+result}</p>
+        </div>
+    )
+}
+
+
+export default EnglishPhrase;
