01094adc981606b06178f66ea033568cf0c78200
Betti Bodnar <bettinabodnar7@gmail.com>
2022-02-05 09:54:42 +0100

    feat: add = operator to numberConversionTo1000 to handle 0 input.

--- arabic-number-conversion-app/src/util/conversion.js
+++ arabic-number-conversion-app/src/util/conversion.js
@@ -1,4 +1,4 @@
-const numbersUnder20AsText = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen"}
+const numbersUnder20AsText = {0:"zero",1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 15:"fifteen", 16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen"}
 const decimals = {20:"twenty",30:"thirty", 40:"forty", 50:'fifty', 60:'sixty',70:'seventy',80:'eighty',90:'ninety'};
 const tenPowersAsText = ["hundred","thousand","million"]
 
@@ -82,7 +82,7 @@ function numberConversionTo1000(placeValueArray,divider, placeValueAsText){
     if(parseSplitNumber>divider) {
         result += getNameOfSplitNumber(division) +" "+ placeValueAsText +" ";
     }
-    if(remainder > 0) {
+    if(remainder >= 0) {
         result += getNameOfSplitNumber(remainder);
     }
     return result;
