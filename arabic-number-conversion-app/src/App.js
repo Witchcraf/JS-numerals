318a1e6e13c5c131942616033109d3b93929b266
Betti Bodnar <bettinabodnar7@gmail.com>
2022-02-05 09:47:37 +0100

    feat: add Title component to App.js

--- arabic-number-conversion-app/src/App.js
+++ arabic-number-conversion-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {useState} from "react";
 import Form from "./components/form"
 import EnglishPhrase from "./components/englishphrase"
+import Title from "./components/header"
 import Matrix from "./components/matrixbackground";
 
 
@@ -9,15 +10,15 @@ function App() {
     const [arabNumber, setArabNumber] = useState(0);
     const [result, setResult] = useState("");
 
-  return (
-      <>
-          <div className="App" >
-              <Matrix />
-              <h1 style={{color:"#F5F5DC", fontFamily: 'MATRIX', fontSize:"50px", marginBottom: "50px"}}>The one conversion app</h1>
-              <Form arabNumber={arabNumber} setArabNumber={setArabNumber} setResult={setResult}/>
-              <EnglishPhrase result={result}/>
-          </div>
-      </>
-  );
-}
+    return (
+            <>
+              <div className="App" >
+                  <Matrix />
+                  <Title title="The one conversion app"/>
+                  <Form arabNumber={arabNumber} setArabNumber={setArabNumber} setResult={setResult}/>
+                  <EnglishPhrase result={result}/>
+              </div>
+            </>
+            );
+    }
 export default App;
