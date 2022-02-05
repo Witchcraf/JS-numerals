fa13f8d976d50e0afcc01c8aa82e8209237ee825
Betti Bodnar <bettinabodnar7@gmail.com>
2022-02-05 09:48:58 +0100

    feat: styled textfield with makestyles helper function.

--- arabic-number-conversion-app/src/components/form.js
+++ arabic-number-conversion-app/src/components/form.js
@@ -1,29 +1,42 @@
 import {convertArabianNumberToEnglishPhrase} from "../util/conversion";
 import styled, {keyframes} from "styled-components";
 import {TextField} from "@mui/material";
+import { makeStyles } from "@material-ui/core/styles";
 
 
 function Form({arabNumber, setArabNumber, setResult}){
+    //helper function to add style to material ui textfield
+    const useStyles = makeStyles(theme => ({
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            width: "30ch",
+            height: "6ch",
+            backgroundColor: "#F5F5DC",
+            borderRadius:"15px"
+        }
+    }));
+    const classes = useStyles();
+
+    //set arabNumber in usestate
     const handleChange = (event) => {
         setArabNumber(event.target.value);
     }
 
-
+    //convert number to phrase and set the result in usestate
     const handleSubmit = (event) => {
         let result = convertArabianNumberToEnglishPhrase(arabNumber);
         setResult(result);
         event.preventDefault();
     }
 
-
     return (
         <form onSubmit={handleSubmit} data-testid="conversionform">
             <TextField id="outlined-basic"
                        variant="outlined"
                        type="number"
-                       inputProps={{ pattern: "[0-9]*",min:1,max:1000000000,
-                           style:{backgroundColor: "#F5F5DC",
-                                   width:"200px"}}}
+                       className={classes.textField}
+                       inputProps={{ pattern: "[0-9]*",min:0,max:1000000000}}
                        value={arabNumber} onChange={(e) => handleChange(e)}
                        required/>
             <br/>
@@ -32,35 +45,32 @@ function Form({arabNumber, setArabNumber, setResult}){
     )
 }
 
-
 const jump = keyframes`
-  from{
+    from{
     transform: translateY(0)
-  }
-  to{
+    }
+    to{
     transform: translateY(-3px)
-  }
+    }
 `;
 
 const Button = styled.button`
-  max-width: 100%;
-  padding: 11px 13px;
-  color: black;
-  font-weight: 600;
-  text-transform: uppercase;
-  background: #66CC00;
-  border: none;
-  border-radius: 3px;
-  outline: 0;
-  cursor: pointer;
-  margin-top: 50px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease-out;
-  :hover {
+    max-width: 100%;
+    padding: 11px 13px;
+    color: black;
+    font-weight: 600;
+    text-transform: uppercase;
+    background: #66CC00;
+    border: none;
+    border-radius: 15px;
+    outline: 0;
+    cursor: pointer;
+    margin-top: 50px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease-out;
+    :hover {
     background: #FFC0CB;
     animation: ${jump} 0.2s ease-out forwards;
-  }
+    }
 `;
-
-
 export default Form;
